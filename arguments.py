64255b775b530352a1c68d2c8e5a5e6545347903
Thien Trang <thientrangngv@gmail.com>
2022-03-22 15:27:00 +0700

    add files

---
+++ arguments.py
@@ -0,0 +1,31 @@
+import argparse
+
+def get_args():
+    parser=argparse.ArgumentParser(description='Continual')
+    parser.add_argument('--seed',type=int,default=0,help='(default=%(default)d)')
+    parser.add_argument('--experiment',default='',type=str,required=True,choices=['mnist2','pmnist','cifar','mixture', 'easy-chasy', 'hard-chasy', 'smnist', 'split_mnist', 'split_cifar100'],help='(default=%(default)s)')
+    parser.add_argument('--approach',default='',type=str,required=True,choices=['random','sgd','sgd-frozen','lwf','lfl','ewc','imm-mean','progressive','pathnet',
+                                                                                'imm-mode','sgd-restart', 'ewc2', 'ewc-film',
+                                                                                'joint','hat','hat-test', 'gvcl', 'vcl', 'vclf', 'gvclf', 'gvclf_vd'],help='(default=%(default)s)')
+    parser.add_argument('--film', action='store_true', default=False, help='Add film layers')
+    parser.add_argument('--batch-size', default=64, type=int, required=False, help='(default=%(default)d)')
+    parser.add_argument('--output',default='',type=str,required=False,help='(default=%(default)s)')
+    parser.add_argument('--nepochs',default=-1,type=int,required=False,help='(default=%(default)d)')
+    parser.add_argument('--lr',default=-1,type=float,required=False,help='(default=%(default)f)')
+    parser.add_argument('--parameter',type=str,default='',help='(default=%(default)s)')
+    parser.add_argument('--ntasks',type=int,default=-1,help='(default=%(default)s)')
+    parser.add_argument('--use-best-hyperparams',type=bool,default=True,help='(default=%(default)s)')
+    parser.add_argument('--drop_type', type=str, default='Gauss', choices= ['Gauss', 'AddNoise'], help='Choose type of dropout') 
+    parser.add_argument('--droprate', type=float, default=0.5, help="Choose dropout rate") 
+    parser.add_argument('--droprate_linear', type=float, default=0.5, help="Choose dropout rate for linear net") 
+    parser.add_argument('--single_head', action='store_true', default=False, help='Use single head')
+    parser.add_argument('--wo_Dropout', action='store_true', default=False, help="without Dropout exp") 
+    parser.add_argument('--conv_Dropout', action='store_true', default=False, help="VD for CNN") 
+    parser.add_argument('--num_samples', type=int, default=1, help='The number of taking sample') 
+    parser.add_argument('--KL_coeff', type=str, choices=['1', '1_M', '1_N', 'M_N'], default='1_N', required=False) 
+    parser.add_argument('--KL_weight', type=float, default=1, help='Choose the KL weight of noise') 
+    parser.add_argument('--test_samples', type=int, default=100, help='Number of sample at test time') 
+
+    args=parser.parse_args()
+
+    return args
---
+++ networks/gvcl_models.py
@@ -0,0 +1,193 @@
+from networks.gvcl_model_classes import MultiHeadFiLMCNN, MultiHeadFiLMCNNVD
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn.parameter import Parameter
+
+import math
+from torch.nn import init
+from functools import partial
+from torch.nn.modules.utils import _pair
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn.parameter import Parameter
+from torch import distributions
+
+import math
+import numpy as np
+from torch.nn import init
+from functools import partial
+
+from arguments import get_args
+
+args = get_args()
+
+class MLPFilmVD:
+    class Net(MultiHeadFiLMCNNVD):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((1,28,28), [], [400,400], heads, [28*28, 400], film_type = 'point')
+        
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            for i, layer in enumerate(self.fc_layers):
+                x = self.fc_dropout_layers[i](x, task_labels, num_samples)
+                x = layer(x)
+                if args.film:
+                    x = self.fc_film_layers[i](x, task_labels, num_samples)
+                x = F.relu(x)
+            return x
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            return x
+
+
+class CNNFilmVD:
+    class Net(MultiHeadFiLMCNNVD):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((3,32,32), [(32, 3, 1), (32, 3, 1), 'pool', (64, 3, 1), (64, 3, 1), 'pool', (128, 3, 1), (128, 3, 1), 'pool'], [256], heads, 
+                            [256], film_type = 'point')
+            if not args.conv_Dropout:     
+                self.drop = nn.Dropout(0.25)
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            drop_index = 0
+            for i, conv_layer in enumerate(self.conv_layers):
+                x = conv_layer(x)
+                if args.film:
+                    x = self.conv_film_layers[i](x, task_labels, num_samples)
+                
+                x = F.relu(x)
+                if i in self.pool_indices:
+                    x = F.max_pool2d(x, kernel_size = 2, stride = 2)
+                    if args.conv_Dropout:
+                        x = self.conv_dropout_layers[drop_index](x, task_labels, num_samples)
+                    else:
+                        x = self.drop(x)
+                    drop_index += 1
+            return x
+
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            for i, layer in enumerate(self.fc_layers):
+                x = layer(x)
+                if args.film:
+                    x = self.fc_film_layers[i](x, task_labels, num_samples)
+                x = F.relu(x)
+                x = self.fc_dropout_layers[i](x, task_labels, num_samples)
+            return x
+
+class CNNOmniglotFilmVD:
+    class Net(MultiHeadFiLMCNNVD):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((1,28,28), [(64, 3, 0), (64, 3, 0), 'pool', (64, 3, 0), (64, 3, 0), 'pool'], [], heads, 
+                                        [1024], film_type = 'point')
+            if not args.conv_Dropout:     
+                self.drop = nn.Dropout(args.droprate_linear)
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            drop_index = 0
+            for i, conv_layer in enumerate(self.conv_layers):
+                x = conv_layer(x)
+                if args.film:
+                    x = self.conv_film_layers[i](x, task_labels, num_samples)
+                
+                x = F.relu(x)
+                if i in self.pool_indices:
+                    x = F.max_pool2d(x, kernel_size = 2, stride = 2)
+                    if args.conv_Dropout:
+                        x = self.conv_dropout_layers[drop_index](x, task_labels, num_samples)
+                    else:
+                        x = self.drop(x)
+                    drop_index += 1
+            return x
+
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            x = self.fc_dropout_layers[0](x, task_labels, num_samples)
+            return x
+
+class MLPFilm:
+    class Net(MultiHeadFiLMCNN):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((1,28,28), [], [400,400], heads, film_type = 'point')
+            if not args.wo_Dropout:
+                self.drop = torch.nn.Dropout(args.droprate_linear)
+        
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            for i, layer in enumerate(self.fc_layers):
+                x = layer(x)
+                if args.film:
+                    x = self.fc_film_layers[i](x, task_labels, num_samples)
+                x = F.relu(x)
+                if not args.wo_Dropout:
+                    x = self.drop(x)
+            return x
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            return x
+
+class CNNOmniglotFilm:
+    class Net(MultiHeadFiLMCNN):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((1,28,28), [(64, 3, 0), (64, 3, 0), 'pool', (64, 3, 0), (64, 3, 0), 'pool'], [], heads, 
+                                        film_type = 'point')
+            if not args.wo_Dropout:
+                self.drop = nn.Dropout(args.droprate_linear)
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            for i, conv_layer in enumerate(self.conv_layers):
+                x = conv_layer(x)
+                if args.film:
+                    x = self.conv_film_layers[i](x, task_labels, num_samples)
+                
+                x = F.relu(x)
+                if i in self.pool_indices:
+                    x = F.max_pool2d(x, kernel_size = 2, stride = 2)
+                    if not args.wo_Dropout:
+                        x = self.drop(x)
+            return x
+
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            if not args.wo_Dropout:
+                x = self.drop(x)
+            return x
+
+class CNNFilm:
+    class Net(MultiHeadFiLMCNN):
+        def __init__(self, inputsize,taskcla):
+            heads = [t[1] for t in taskcla]
+            super().__init__((3,32,32), [(32, 3, 1), (32, 3, 1), 'pool', (64, 3, 1), (64, 3, 1), 'pool', (128, 3, 1), (128, 3, 1), 'pool'], [256], heads, 
+                                        film_type = 'point')
+            if not args.wo_Dropout:
+                self.drop1 = nn.Dropout(0.25)
+                self.drop2 = nn.Dropout(0.5)
+
+        def forward_conv(self, x, task_labels, num_samples=1, tasks = None):
+            for i, conv_layer in enumerate(self.conv_layers):
+                x = conv_layer(x)
+                if args.film:
+                    x = self.conv_film_layers[i](x, task_labels, num_samples)
+                
+                x = F.relu(x)
+                if i in self.pool_indices:
+                    x = F.max_pool2d(x, kernel_size = 2, stride = 2)
+                    if not args.wo_Dropout:
+                        x = self.drop1(x)
+            return x
+
+        def forward_linear(self, x, task_labels, num_samples=1, tasks = None):
+            for i, layer in enumerate(self.fc_layers):
+                x = layer(x)
+                if args.film:
+                    x = self.fc_film_layers[i](x, task_labels, num_samples)
+                x = F.relu(x)
+                if not args.wo_Dropout:
+                    x = self.drop2(x)
+            return x
+
+
+
